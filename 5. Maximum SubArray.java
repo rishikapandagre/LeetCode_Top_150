89ba9beaba9b2ca03378a3e30e7dcda82975eed6
Rishika Pandagre <pandagrerishika0195@gmail.com>
2026-03-20 15:16:40 +0530

    -

---
+++ 5. Maximum SubArray.java
@@ -0,0 +1,17 @@
+// LeetCode - 53
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currsum = nums[0];
+        int maxsum = nums[0];
+        for (int i=1; i<nums.length; i++){
+            if(currsum + nums[i] > nums[i]){
+                currsum+=nums[i];
+            }else {
+                currsum = nums[i];
+            }
+            maxsum = Math.max(currsum , maxsum);
+        }
+        return maxsum;
+    }
+}
