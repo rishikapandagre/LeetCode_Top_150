fcb500beb9af826fcb2e33f68d3f5d2005b8f9d7
Rishika Pandagre <pandagrerishika0195@gmail.com>
2026-03-20 15:28:33 +0530

    -

---
+++ 9. 3Sum.java
@@ -0,0 +1,43 @@
+// LeetCode - 15
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        /*
+        for (int i=0; i < nums.length; i++){
+            int left = i+1;
+            int right = nums.length - 1;
+            while ( i != left && i != right && left != right){
+                if (nums[i] + nums[left] + nums[right] == 0){
+                    return new int[] {nums[i], nums[left], nums[right]};
+                }
+            }
+        }
+        return new int[0];
+        */
+        
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i=0; i<n-2; i++){
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1; 
+            int k=n-1;
+            while (j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0){
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j<k && nums[j] == nums[j-1]) j++;
+                    while (j<k && nums[k] == nums[k+1]) k--;
+                }else if(sum<0){
+                    j++;
+                }else{
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+}
